945277fb2282a83b645196c52d719af82ac2c432
marcincuber <mcuber@dare.global>
2023-05-20 09:20:52 +0100

    Add support for create-before-destroy plus upgrades

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "iam_role_arn" {
   description = "IAM role ARN used by node group."
-  value       = join("", aws_iam_role.main.*.arn)
+  value       = try(aws_iam_role.main[0].arn, null)
 }
 
 output "iam_role_id" {
   description = "IAM role ID used by node group."
-  value       = join("", aws_iam_role.main.*.id)
+  value       = try(aws_iam_role.main[0].id, null)
 }
 
 output "node_group" {
   description = "Outputs from EKS node group. See `aws_eks_node_group` Terraform documentation for values"
-  value       = aws_eks_node_group.main
+  value       = var.create_before_destroy ? try(aws_eks_node_group.main_create_before_destroy[0], null) : try(aws_eks_node_group.main[0], null)
 }
